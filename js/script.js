77f9d3f38ab80f3245e5a325d71a6c7314867a19
Igor Santos <igornzds@gmail.com>
2020-10-22 20:25:11 -0300

    Finalizar script para o menu mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('testando');
+function toggleMenuMobile() {
+    const btnMenu = document.querySelector('[data-menu="btn"]');
+    const menu = document.querySelector('[data-menu="menu"]');
+
+    function toggleMenu() { 
+        menu.classList.toggle('active');
+        btnMenu.classList.toggle('active');
+    }
+
+    btnMenu.addEventListener('click', toggleMenu);
+}   
+
+toggleMenuMobile();
