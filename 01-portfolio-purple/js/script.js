6c4d4789dbd427668d66a561172b2d75d3bc7eb9
Igor Santos <igornzds@gmail.com>
2021-01-06 23:59:50 -0300

    [PROJETO-01] Inicial modal

--- 01-portfolio-purple/js/script.js
+++ 01-portfolio-purple/js/script.js
@@ -1,5 +1,7 @@
 import ToggleMenuMobile from './modules/menuMobile.js';
 import animationScroll from './modules/scrollAnimation.js';
+import modal from './modules/modal.js';
 
 ToggleMenuMobile();
 animationScroll();
+modal();
