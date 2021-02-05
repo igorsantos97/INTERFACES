27bb45c2abae5e1e090129d055ddf77a3b026553
Igor Santos <igornzds@gmail.com>
2021-02-05 16:59:48 -0300

    [PROJETO-01] Finalizar modal do site

--- 01-portfolio-purple/js/modules/modal.js
+++ 01-portfolio-purple/js/modules/modal.js
@@ -1,1 +1,15 @@
-export default function modal() {}
+export default function modal() {
+  const containerModal = document.querySelector('[data-modal="container"]');
+  const openBtn = document.querySelector('[data-modal="abrir"]');
+  const closeBtn = document.querySelector('[data-modal="fechar"]');
+
+  function toggleModal() {
+    containerModal.classList.toggle('active');
+  }
+
+  openBtn.addEventListener('click', toggleModal);
+  closeBtn.addEventListener('click', toggleModal);
+  containerModal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal-container')) toggleModal();
+  });
+}
